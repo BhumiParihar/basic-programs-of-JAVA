f5597379fb5bc58e2b81c4d8b769a5e13777fb5c
Bhumi Parihar <123962969+BhumiParihar@users.noreply.github.com>
2023-04-25 22:04:31 +0530

    Add files via upload

---
+++ arithemetic operators.java
@@ -0,0 +1,19 @@
+// Program to implement Arithmetic Operations:
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    int a=5,b=6,c=7,d=8;
+	    int sum,difference,multiplication,divide,remainder;
+	    sum=a+b;
+	    difference=c-d;
+	    multiplication=b*c;
+	    divide=d/a;
+	    remainder=a%b;
+	    System.out.println("sum of numbers is : "+sum +"\n");
+	    System.out.println("Difference of numbers is : "+difference +"\n");
+	    System.out.println("multiplication of numbers is : "+multiplication +"\n");
+	    System.out.println("division of numbers is : "+divide +"\n");
+	    System.out.println("remainder of numbers is : "+remainder +"\n");
+	}
+}

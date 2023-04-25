6768f1d42bb33ab9f170e2b27bb874f12d70a87c
Abhidha Joshi <124055802+Abhidhajoshi@users.noreply.github.com>
2023-04-25 22:51:03 +0530

    Add files via upload

---
+++ unary_operators.java
@@ -0,0 +1,21 @@
+//program to explain the functionality of unary operators.
+
+public class Main
+{
+	public static void main(String[] args) {
+		int a=10;
+		//pre-increment
+		++a;
+		System.out.println("pre increment: "+a);
+		//pre-decrement
+		--a;
+		System.out.println("pre decrement: "+a);
+		//post-increment
+		a++;
+		System.out.println("post increment: "+a);
+		//post-decrement
+		a--;
+		System.out.println("post decrement: "+a);
+		
+	}
+}
